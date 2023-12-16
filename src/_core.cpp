d18e9637edc83ef0c595bde8dde21a880d39e040
Kian Orr <orrk931@gmail.com>
2023-12-15 20:32:12 -0500

    added to _core.cpp

--- src/_core.cpp
+++ src/_core.cpp
@@ -7,5 +7,11 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
+  py::class_<HarmonicOscillator>(m, "HarmonicOscillator");
+    .def(py::init<>());
+    .def("f", &HarmonicOscillator::f);
+    // Not sure about this part
+    .def("__repr__", [](py::object self){
+            return py::str("{0.__class__.__name__}({0.f})").format(self);
+        })
 }
