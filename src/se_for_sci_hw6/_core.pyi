842c83183e4942b3aeebba4926e5c072a527137f
Kian Orr <orrk931@gmail.com>
2023-12-15 20:36:05 -0500

    formatting

--- src/se_for_sci_hw6/_core.pyi
+++ src/se_for_sci_hw6/_core.pyi
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 import numpy.typing
 
-
 class HarmonicOscillator:
     def integrate(
         self, stepsize: float, steps: int, init_y: tuple[float, float]
-    ) -> numpy.typing.NDArray[numpy.float64]:
-        ...
+    ) -> numpy.typing.NDArray[numpy.float64]: ...
